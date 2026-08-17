e15e93e286603f5f600b31918c6c3c512d855be3
ajay kale <ajaykale749810@gmail.com>
2026-08-17 16:22:19 +0530

    Count Doublicate Element

---
+++ Array/Array/CountDublicateElement.cs
@@ -0,0 +1,21 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 1, 2, 3, 2, 4, 5, 1, 2 };
+        int count = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[i] == arr[j])
+                {
+                    count++;
+                    break;
+                }
+            }
+        }
+        Console.WriteLine("Duplicate elements: " + count);
+    }
+}
